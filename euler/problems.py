6dc83128454fdefbac69b71ed5d7f1d24e8ad5e2
Kyle Shores <kyle.shores44@gmail.com>
2021-04-03 20:44:07 -0500

    fixing divisors function

--- euler/problems.py
+++ euler/problems.py
@@ -120,7 +120,7 @@ def is_coprime(a, b):
 def get_divisors(n):
     if n == 0:
         return []
-    divisors = [(i, n // i) for i in range(1, int(np.sqrt(n))) if n % i == 0 and i != n // i]
+    divisors = [(i, n // i) for i in range(1, int(np.sqrt(n)) + 1) if n % i == 0 and i != n // i]
     divisors = [j for i in divisors for j in i]
     if int(np.sqrt(n)) ** 2 == n:
         divisors.append(int(np.sqrt(n)))
@@ -1033,7 +1033,6 @@ class Problems:
 
         return amicable_sum
 
-    @current
     @register_problem
     def __22(self):
         """\thttps://projecteuler.net/problem=22
@@ -1080,6 +1079,38 @@ class Problems:
             total += score
 
         return total
+
+    @current
+    @register_problem
+    def __22(self):
+        """\thttps://projecteuler.net/problem=23
+        A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the
+        proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+
+        A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
+
+        As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant
+        numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two
+        abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest 
+        number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+
+        Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+
+        """
+
+        def is_abundant(n):
+            s = sum(get_divisors(n)) - n
+            return s > n
+
+        def is_perfect(n):
+            s = sum(get_divisors(n)) - n
+            return s == n
+
+        def is_defficient(n):
+            s = sum(get_divisors(n)) - n
+            return s < n
+
+        return 0
 
     #endregion
 
