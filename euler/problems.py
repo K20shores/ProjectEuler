0a07bbf1ff9ed362f9b8c4720b4b05274df3f62b
Kyle Shores <kyle.shores44@gmail.com>
2021-04-04 22:01:38 -0500

    finished number 25

--- euler/problems.py
+++ euler/problems.py
@@ -1134,7 +1134,6 @@ class Problems:
         return s - sum(invalid.keys())
         # return ns.sum()
 
-    @current
     @register_problem
     def __24(self):
         """\thttps://projecteuler.net/problem=24
@@ -1160,6 +1159,42 @@ class Problems:
 
         perms = permutations([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
         return ''.join(map(str, perms[int(1e6 - 1)]))
+
+    @current
+    @register_problem
+    def __25(self):
+        """\thttps://projecteuler.net/problem=25
+        The Fibonacci sequence is defined by the recurrence relation:
+
+            Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+
+        Hence the first 12 terms will be:
+
+            F1 = 1
+            F2 = 1
+            F3 = 2
+            F4 = 3
+            F5 = 5
+            F6 = 8
+            F7 = 13
+            F8 = 21
+            F9 = 34
+            F10 = 55
+            F11 = 89
+            F12 = 144
+
+        The 12th term, F12, is the first term to contain three digits.
+
+        What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+        """
+
+        a, b = 1, 1
+        j = 1
+        while (len(str(a))) != 1000:
+            a, b = b, a + b
+            j += 1
+
+        return j
 
     #endregion
 
