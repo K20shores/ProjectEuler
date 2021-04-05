518f87aaebe605147c89e2ae9166ba55c76df415
Kyle Shores <kyle.shores44@gmail.com>
2021-04-04 21:57:10 -0500

    finished number 24

--- euler/problems.py
+++ euler/problems.py
@@ -1080,7 +1080,6 @@ class Problems:
 
         return total
 
-    @current
     @register_problem
     def __23(self):
         """\thttps://projecteuler.net/problem=23
@@ -1134,6 +1133,33 @@ class Problems:
 
         return s - sum(invalid.keys())
         # return ns.sum()
+
+    @current
+    @register_problem
+    def __24(self):
+        """\thttps://projecteuler.net/problem=24
+        A permutation is an ordered arrangement of objects. For example, 3124 is one possible
+        permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically 
+        or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 
+        and 2 are:
+
+        012   021   102   120   201   210
+
+        What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+        """
+
+        def permutations(ls):
+            if len(ls) == 1:
+                return [ls]
+            perms = []
+            for i, iv in enumerate(ls):
+                rest = ls[:i] + ls[i+1:]
+                for perm in permutations(rest):
+                    perms.append([iv] + perm)
+            return perms
+
+        perms = permutations([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        return ''.join(map(str, perms[int(1e6 - 1)]))
 
     #endregion
 
